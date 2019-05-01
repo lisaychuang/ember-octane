e4abe75a10e59bc6a15977ca8cc05aadaab2010b
Lisa Huang <lisa.yc.huang@gmail.com>
2019-05-01 14:05:50 -0500

    Guard routes with beforeModel hook

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
+import {inject as service } from '@ember/service'
+import AuthService from 'shlack/services/auth';
 
 export default class LoginRoute extends Route {
+    /**
+     * 
+     * @type {AuthService}
+     */
+    @service auth
+
+    // Use route hook to GUARD routes
+    // route's life cycle hook to validate entry into this route
+    // gets a 'transition' object as arg
+    async beforeModel(transition){
+        // do the default Ember stuff in beforeModel
+        await super.beforeModel(transition); 
+
+        // if no current user, go to the Login screen
+        if (this.auth.currentUserId) { 
+            this.transitionTo('teams')
+        }
+    }
 }
