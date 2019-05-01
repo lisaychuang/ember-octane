d146233b36569a47b65b10292be9f37b073c8ed4
Lisa Huang <lisa.yc.huang@gmail.com>
2019-05-01 11:20:53 -0500

    Add Logout acceptance test

---
+++ tests/acceptance/logout-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click, pauseTest } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | logging out', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /teams and clicking "Logout" ', async function(assert) {
+    await visit('/teams'); // Go to /teams url
+    // await this.pauseTest();
+    assert.equal(currentURL(), '/teams');
+
+    await click('.team-sidebar__logout-button'); // Click on "Logout" button
+    assert.equal(currentURL(), '/login');
+
+  });
+});
